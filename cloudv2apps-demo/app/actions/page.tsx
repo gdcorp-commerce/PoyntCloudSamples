9521caf4efa6e416a2ff428ae834f9be25f3709a
amiranda-godaddy <amiranda@godaddy.com>
2023-10-24 12:55:33 -0300

    added localstorage

--- cloudv2apps-demo/app/actions/page.tsx
+++ cloudv2apps-demo/app/actions/page.tsx
@@ -8,6 +8,7 @@ export default function Actions() {
     client_secret: clientSecret,
     env,
     stores,
+    loadingStores,
   } = useResources();
   const [orderName, setNewOrderName] = useState<string>("");
   const [orderQuantity, setOrderQuantity] = useState<number>(0);
@@ -68,180 +69,193 @@ export default function Actions() {
   };
 
   useEffect(() => {
-    getOrders();
-  }, []);
+    if (storeId) {
+      getOrders();
+    }
+  }, [storeId]);
+
+  useEffect(() => {
+    if (stores && stores.length > 0) {
+      setStoreId(stores[0]);
+      setStoreIdToCreate(stores[0]);
+    }
+  }, [stores]);
 
   return (
-    <div className="p-10">
-      <h1 className="text-white mb-10">Actions</h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          gap: "100px",
-        }}
-      >
+    <>
+      <div className="p-10">
+        <h1 className="text-white mb-10">Actions</h1>
         <div
           style={{
-            width: "30%",
+            display: "flex",
+            justifyContent: "space-between",
+            gap: "100px",
           }}
-          className="h-38 border border-white p-4 rounded"
         >
-          <h1 className="underline mb-2">Create Order</h1>
-
-          <div className="flex gap-3">
-            <h3 className="text-white mb-2">Store ID</h3>
-
-            <select
-              className="bg-transparent text-white border-white border w-72 mb-2"
-              value={storeIdToCreate}
-              onChange={(e) => setStoreIdToCreate(e.target.value)}
-            >
-              {stores.map((store, index) => {
-                return <option key={index}>{store}</option>;
-              })}
-            </select>
-          </div>
-
-          <div>
-            <h3 className="text-white mb-2">Order Name</h3>
-            <input
-              className="border mb-2 p-1 w-full text-black"
-              placeholder="Create an order"
-              onChange={(e) => setNewOrderName(e.target.value)}
-              value={orderName}
-            />
-          </div>
-
-          <div>
-            <h3 className="text-white mb-2">Price</h3>
-            <input
-              className="border mb-2 p-1 w-full text-black"
-              type="number"
-              value={orderPrice}
-              onChange={(e) => setOrderPrice(parseInt(e.target.value))}
-            />
-          </div>
-
-          <div>
-            <h3 className="text-white mb-2">Quantity</h3>
-            <input
-              className="border mb-2 p-1 w-full text-black"
-              type="number"
-              value={orderQuantity}
-              onChange={(e) => setOrderQuantity(parseInt(e.target.value))}
-            />
-          </div>
-
-          <div>
-            <h3 className="text-white mb-2">Discount</h3>
-            <input
-              className="border mb-2 p-1 w-full text-black"
-              type="number"
-              value={orderDiscount}
-              onChange={(e) => setOrderDiscount(parseInt(e.target.value))}
-            />
-          </div>
-
-          <button
-            disabled={loadingCreate}
-            onClick={() => createOrder()}
-            className="bg-transparent border border-white text-white px-12 py-3 rounded hover:bg-white hover:border-black hover:text-black cursor-pointer"
+          <div
+            style={{
+              width: "30%",
+            }}
+            className="h-38 border border-white p-4 rounded"
           >
-            Submit
-          </button>
-        </div>
-        <div
-          style={{
-            width: "100%",
-          }}
-          className="h-38 border border-white p-4 rounded flex flex-col"
-        >
-          <h1 className="underline mb-2">Read Order</h1>
-          <div className="flex gap-3">
-            <h3 className="text-white mb-2">Store ID</h3>
+            <h1 className="underline mb-2">Create Order</h1>
 
-            <select
-              className="bg-transparent text-white border-white border w-72 mb-2"
-              value={storeId}
-              onChange={(e) => onSelectStoreId(e)}
-            >
-              {stores.map((store, index) => {
-                return <option key={index}>{store}</option>;
-              })}
-            </select>
+            <div className="flex gap-3">
+              <h3 className="text-white mb-2">Store ID</h3>
+
+              <select
+                className="bg-transparent text-white border-white border w-72 mb-2"
+                value={storeIdToCreate}
+                onChange={(e) => setStoreIdToCreate(e.target.value)}
+              >
+                {stores.map((store, index) => {
+                  return <option key={index}>{store}</option>;
+                })}
+              </select>
+            </div>
+
+            <div>
+              <h3 className="text-white mb-2">Order Name</h3>
+              <input
+                className="border mb-2 p-1 w-full text-black"
+                placeholder="Create an order"
+                onChange={(e) => setNewOrderName(e.target.value)}
+                value={orderName}
+              />
+            </div>
+
+            <div>
+              <h3 className="text-white mb-2">Price</h3>
+              <input
+                className="border mb-2 p-1 w-full text-black"
+                type="number"
+                value={orderPrice}
+                onChange={(e) => setOrderPrice(parseInt(e.target.value))}
+              />
+            </div>
+
+            <div>
+              <h3 className="text-white mb-2">Quantity</h3>
+              <input
+                className="border mb-2 p-1 w-full text-black"
+                type="number"
+                value={orderQuantity}
+                onChange={(e) => setOrderQuantity(parseInt(e.target.value))}
+              />
+            </div>
+
+            <div>
+              <h3 className="text-white mb-2">Discount</h3>
+              <input
+                className="border mb-2 p-1 w-full text-black"
+                type="number"
+                value={orderDiscount}
+                onChange={(e) => setOrderDiscount(parseInt(e.target.value))}
+              />
+            </div>
 
             <button
-              style={{ width: "50px", height: "22px" }}
-              onClick={() => getOrders()}
-              className="bg-transparent border border-white text-white rounded hover:bg-white hover:border-black hover:text-black cursor-pointer"
+              disabled={loadingCreate}
+              onClick={() => createOrder()}
+              className="bg-transparent border border-white text-white px-12 py-3 rounded hover:bg-white hover:border-black hover:text-black cursor-pointer"
             >
-              Read
+              Submit
             </button>
           </div>
+          <div
+            style={{
+              width: "100%",
+            }}
+            className="h-38 border border-white p-4 rounded flex flex-col"
+          >
+            <h1 className="underline mb-2">Read Order</h1>
+            <div className="flex gap-3">
+              <h3 className="text-white mb-2">Store ID</h3>
 
-          {!loading && orders.length === 0 ? (
-            <h1 className="text-white mt-2">No orders</h1>
-          ) : loading ? (
-            <h1 className="text-white mt-2">Loading...</h1>
-          ) : (
-            <div className="overflow-x-auto">
-              <table className="min-w-full border border-gray-300">
-                <thead>
-                  <tr>
-                    <th className="border border-gray-300">ID</th>
-                    <th className="border border-gray-300">
-                      Fulfillment Status
-                    </th>
-                    <th className="border border-gray-300">Payment Status</th>
-                    <th className="border border-gray-300">Status</th>
-                    <th className="border border-gray-300">Discount Total</th>
-                    <th className="border border-gray-300">Fee Total</th>
-                    <th className="border border-gray-300">Shipping Total</th>
-                    <th className="border border-gray-300">Sub Total</th>
-                    <th className="border border-gray-300">Tax Total</th>
-                    <th className="border border-gray-300">Total</th>
-                    <th className="border border-gray-300">Currency Code</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders.map((item: any, index: any) => (
-                    <tr key={index}>
-                      <td className="border border-gray-300">{item.id}</td>
-                      <td className="border border-gray-300">
-                        {item.fulfillmentStatus}
-                      </td>
-                      <td className="border border-gray-300">
-                        {item.paymentStatus}
-                      </td>
-                      <td className="border border-gray-300">{item.status}</td>
-                      <td className="border border-gray-300">
-                        {item.discountTotal}
-                      </td>
-                      <td className="border border-gray-300">
-                        {item.feeTotal}
-                      </td>
-                      <td className="border border-gray-300">
-                        {item.shippingTotal}
-                      </td>
-                      <td className="border border-gray-300">
-                        {item.subTotal}
-                      </td>
-                      <td className="border border-gray-300">
-                        {item.taxTotal}
-                      </td>
-                      <td className="border border-gray-300">{item.total}</td>
-                      <td className="border border-gray-300">
-                        {item.currencyCode}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+              <select
+                className="bg-transparent text-white border-white border w-72 mb-2"
+                value={storeId}
+                onChange={(e) => onSelectStoreId(e)}
+              >
+                {stores.map((store, index) => {
+                  return <option key={index}>{store}</option>;
+                })}
+              </select>
+
+              <button
+                style={{ width: "50px", height: "22px" }}
+                onClick={() => getOrders()}
+                className="bg-transparent border border-white text-white rounded hover:bg-white hover:border-black hover:text-black cursor-pointer"
+              >
+                Read
+              </button>
             </div>
-          )}
+
+            {!loading && orders?.length === 0 ? (
+              <h1 className="text-white mt-2">No orders</h1>
+            ) : loading ? (
+              <h1 className="text-white mt-2">Loading...</h1>
+            ) : (
+              <div className="overflow-x-auto">
+                <table className="min-w-full border border-gray-300">
+                  <thead>
+                    <tr>
+                      <th className="border border-gray-300">ID</th>
+                      <th className="border border-gray-300">
+                        Fulfillment Status
+                      </th>
+                      <th className="border border-gray-300">Payment Status</th>
+                      <th className="border border-gray-300">Status</th>
+                      <th className="border border-gray-300">Discount Total</th>
+                      <th className="border border-gray-300">Fee Total</th>
+                      <th className="border border-gray-300">Shipping Total</th>
+                      <th className="border border-gray-300">Sub Total</th>
+                      <th className="border border-gray-300">Tax Total</th>
+                      <th className="border border-gray-300">Total</th>
+                      <th className="border border-gray-300">Currency Code</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {orders?.map((item: any, index: any) => (
+                      <tr key={index}>
+                        <td className="border border-gray-300">{item.id}</td>
+                        <td className="border border-gray-300">
+                          {item.fulfillmentStatus}
+                        </td>
+                        <td className="border border-gray-300">
+                          {item.paymentStatus}
+                        </td>
+                        <td className="border border-gray-300">
+                          {item.status}
+                        </td>
+                        <td className="border border-gray-300">
+                          {item.discountTotal}
+                        </td>
+                        <td className="border border-gray-300">
+                          {item.feeTotal}
+                        </td>
+                        <td className="border border-gray-300">
+                          {item.shippingTotal}
+                        </td>
+                        <td className="border border-gray-300">
+                          {item.subTotal}
+                        </td>
+                        <td className="border border-gray-300">
+                          {item.taxTotal}
+                        </td>
+                        <td className="border border-gray-300">{item.total}</td>
+                        <td className="border border-gray-300">
+                          {item.currencyCode}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- cloudv2apps-demo/context/resources.tsx
+++ cloudv2apps-demo/context/resources.tsx
@@ -1,9 +1,6 @@
-"use client";
-import CommerceSdk from "@gdcorp-commerce/commerce-sdk";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 
 interface ResourcesContextProps {
-  store_id: string;
   scope: string;
   client_id: string;
   client_secret: string;
@@ -13,6 +10,7 @@ interface ResourcesContextProps {
   appId: string;
   scopes: string[];
   stores: string[];
+  loadingStores: boolean;
 }
 
 interface ResourcesProviderProps {
@@ -24,48 +22,58 @@ export const ResourcesContext = createContext<ResourcesContextProps>(
 );
 
 export const ResourcesProvider = ({ children }: ResourcesProviderProps) => {
-  // miguel store id
   const appName = "quicktest11";
   const appId = "urn:aid:5927acda-f584-4723-97ed-e29bb3f3eb9c";
-  const storeId = "acf9f7fa-b4d8-4a64-aac6-f11000c1c63b";
   const clientId = "c9c62f88-0b3f-430c-8b50-3e375f9cc7a1";
   const clientSecret = "RyEiVQJawqVCjiPgh2LJAHp0GEwZSOWC";
   const env = "development";
   const ssoUri = `https://sso.dev-godaddy.com/authorize?client_id=${clientId}&scope=commerce.order:read%20commerce.order:create&redirect_uri=https%3A%2F%2Fwww.bar.com`;
-  const [stores, setStores] = useState<string[]>([
-    "acf9f7fa-b4d8-4a64-aac6-f11000c1c63b",
-    "4c5e697f-400a-412c-b6b7-dedcc17602ff",
-  ]);
-  const [scopes, setScopes] = useState<string[]>([
-    "commerce.order:create",
-    "commerce.order:read",
-  ]);
+  const [stores, setStores] = useState<string[]>([]);
+  const [scopes, setScopes] = useState<string[]>(["default.scope.value"]); // Default value for scopes
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setLoading(true);
     if (window.location.pathname.includes("actions")) {
-      const { scopes, storeIds } = extractIdsAndScopesFromCurrentURL();
-      if (storeIds && scopes) {
-        setStores(storeIds);
-        setScopes(scopes);
+      const storeIdsFromLocalStorage = localStorage.getItem("storeIds");
+      if (storeIdsFromLocalStorage) {
+        const parsedStores = JSON.parse(storeIdsFromLocalStorage);
+        if (JSON.stringify(parsedStores) !== JSON.stringify(stores)) {
+          setStores(parsedStores);
+        }
+      } else if (!window.location.search.includes("resourceIds")) {
+        window.location.href = "/";
+      } else if (window.location.search.includes("resourceIds")) {
+        const { scopes: extractedScopes, storeIds } =
+          extractIdsAndScopesFromCurrentURL();
+
+        if (storeIds && JSON.stringify(storeIds) !== JSON.stringify(stores)) {
+          localStorage.setItem("storeIds", JSON.stringify(storeIds));
+          setStores(storeIds);
+        }
+
+        if (extractedScopes && extractedScopes.length > 0) {
+          setScopes(extractedScopes);
+        }
       }
     }
+    setLoading(false);
   }, []);
 
   const context = useMemo(
     () => ({
-      store_id: storeId,
       ssoUri,
       client_id: clientId,
       client_secret: clientSecret,
       env,
-      //scope,
       scope: "commerce.order:read",
       appName,
       appId,
       scopes,
       stores,
+      loadingStores: loading,
     }),
-    []
+    [stores, scopes]
   );
 
   return (
@@ -86,26 +94,17 @@ export const useResources = () => {
 };
 
 function extractIdsAndScopesFromCurrentURL() {
-  // const url = window.location.href;
-  // const url =
-  //   "https://www.bar.com/?resourceIds=acf9f7fa-b4d8-4a64-aac6-f11000c1c63b%2C4c5e697f-400a-412c-b6b7-dedcc17602ff&scope=commerce.order%3Acreate%2Ccommerce.order%3Aread&success=true";
-  // Extract resourceIds from the URL
-  const resourceIdsParam = new URLSearchParams(
-    new URL(
-      "https://www.bar.com/?resourceIds=acf9f7fa-b4d8-4a64-aac6-f11000c1c63b%2C4c5e697f-400a-412c-b6b7-dedcc17602ff&scope=commerce.order%3Acreate%2Ccommerce.order%3Aread&success=true"
-    ).search
-  ).get("resourceIds");
+  const url = window.location.href;
+
+  const resourceIdsParam = new URLSearchParams(new URL(url).search).get(
+    "resourceIds"
+  );
   const storeIds = resourceIdsParam?.split(",");
 
-  // Extract scopes from the URL
-  const scopeParam = new URLSearchParams(
-    new URL(
-      "https://www.bar.com/?resourceIds=acf9f7fa-b4d8-4a64-aac6-f11000c1c63b%2C4c5e697f-400a-412c-b6b7-dedcc17602ff&scope=commerce.order%3Acreate%2Ccommerce.order%3Aread&success=true"
-    ).search
-  ).get("scope");
-  const scopes = scopeParam
+  const scopeParam = new URLSearchParams(new URL(url).search).get("scope");
+  const extractedScopes = scopeParam
     ?.split(",")
     .map((scope) => decodeURIComponent(scope));
 
-  return { storeIds, scopes };
+  return { storeIds, scopes: extractedScopes || [] }; // Return empty array if no scopes are found
 }
